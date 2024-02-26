36c2ca2f3f10463c45456347e7a6182ec659d79f
emilianogastaldo <emiliano0303@gmail.com>
2024-02-26 15:06:25 +0100

    added id to class category

--- Models/Category.php
+++ Models/Category.php
@@ -1,18 +1,24 @@
 <?php
 
 class Category{
+    private $id;
     private $category;
 
     public function __construct ($category){
+        $this->id = uniqid();
         $this->category = $category;
     }
 
     public function getCategory(){
         return $this->category;
     }
-
+  
     public function setCategory(string $newCategory){
         $this->category = $newCategory;
+    }
+
+    public function getID(){
+        return $this->id;
     }
 }
 ?>
