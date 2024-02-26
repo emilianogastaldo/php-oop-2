e1f9aa76aef94a6a87d68f51acd75da804643f9c
emilianogastaldo <emiliano0303@gmail.com>
2024-02-26 16:31:31 +0100

    created Product and products

--- Models/Product.php
+++ Models/Product.php
@@ -1,1 +1,27 @@
-<?php?>
+<?php
+
+include_once __DIR__ . '/Category.php';
+
+class Product {
+    public $id;
+    public $category;
+    public $type;
+    public $name;
+    public $image;
+    private $price;
+
+    public function __construct(Category $category, $type, $name, $image, $price ){
+        $this->id = uniqid();
+        $this->category = $category;
+        $this->type = $type;
+        $this->name = $name;
+        $this->image = $image;
+        $this->price = $price;
+    }
+    
+    public function setPrice($price){
+        $this->price = $price;
+    }
+}
+
+?>
--- data/products.php
+++ data/products.php
@@ -1,1 +1,20 @@
-<?php?>
+<?php
+
+include_once __DIR__ . '/../Models/Product.php';
+include_once __DIR__ . '/categories.php';
+
+// __construct(Category $category, $type, $name, $image, $price )
+
+$royal_food = new Product($dog, 'food', 'Royal Canin Mini Adult','https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', rand(1,100));
+$almo_dog_food = new Product($dog, 'food', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso','https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', rand(1,100));
+$almo_cat_food = new Product($cat, 'food', 'Almo Nature Cat Daily Lattina','https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', rand(1,100));
+$fish_food = new Product($fish, 'food', 'Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', rand(1,100));
+$bird_cage = new Product($bird, 'accessory', 'Voliera Wilma in Legno', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', rand(1,100));
+$fish_filter = new Product($fish, 'accessory', 'Cartucce Filtranti per Filtro EasyCrystal', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', rand(1,100));
+$dog_toy = new Product($dog, 'toy', 'Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', rand(1,100));
+$cat_toy = new Product($cat, 'toy', 'Topini di peluche Trixie' ,'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', rand(1,100));
+
+$products = [$royal_food, $almo_dog_food, $almo_cat_food, $fish_food, $bird_cage, $fish_filter, $dog_toy, $cat_toy];
+
+var_dump($products);
+?>
