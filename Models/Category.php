67aa5e7697f9d969315997dd5ead0554d7c29cf3
emilianogastaldo <emiliano0303@gmail.com>
2024-02-26 15:04:11 +0100

    created four categories

--- Models/Category.php
+++ Models/Category.php
@@ -1,1 +1,18 @@
-<?php?>
+<?php
+
+class Category{
+    private $category;
+
+    public function __construct ($category){
+        $this->category = $category;
+    }
+
+    public function getCategory(){
+        return $this->category;
+    }
+
+    public function setCategory(string $newCategory){
+        $this->category = $newCategory;
+    }
+}
+?>
--- data/categories.php
+++ data/categories.php
@@ -1,1 +1,9 @@
-<?php?>
+<?php
+
+include_once __DIR__ . '/../Models/Category.php';
+
+$dog = new Category('Cane');
+$cat = new Category('Gatto');
+$fish = new Category('Pesce');
+$bird = new Category('Uccello');
+?>
