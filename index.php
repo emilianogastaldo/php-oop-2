514224a7a77d07a17b9dcdb1a444741d42ae4611
emilianogastaldo <emiliano0303@gmail.com>
2024-02-26 14:49:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo 'prova';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 2</title>
+</head>
+<body>
+    <footer>
+        <a href="http://localhost/">Torna indietro</a>
+    </footer>
+</body>
+</html>
